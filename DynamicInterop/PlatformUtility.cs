e8b52dbe1c790af9bead47c2e0668182c630564b
J-M <jean-michel.perraud@csiro.au>
2014-11-18 17:18:15 +1100

    Start to refactor DLL loading out of R.NET for more general use.

---
+++ DynamicInterop/PlatformUtility.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DynamicInterop
+{
+    public static class PlatformUtility
+    {
+        /// <summary>
+        /// Is the platform a unix like (Unix or MacOX)
+        /// </summary>
+        public static bool IsUnix
+        {
+            get
+            {
+                var p = GetPlatform();
+                return p == PlatformID.MacOSX || p == PlatformID.Unix;
+            }
+        }
+
+        /// <summary>
+        /// Gets the platform on which the current process runs.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="Environment.OSVersion"/>'s platform is not <see cref="PlatformID.MacOSX"/> even on Mac OS X.
+        /// This method returns <see cref="PlatformID.MacOSX"/> when the current process runs on Mac OS X.
+        /// This method uses UNIX's uname command to check the operating system,
+        /// so this method cannot check the OS correctly if the PATH environment variable is changed (will returns <see cref="PlatformID.Unix"/>).
+        /// </remarks>
+        /// <returns>The current platform.</returns>
+        public static PlatformID GetPlatform()
+        {
+            if (!curPlatform.HasValue)
+            {
+                var platform = Environment.OSVersion.Platform;
+                if (platform != PlatformID.Unix)
+                {
+                    curPlatform = platform;
+                }
+                else
+                {
+                    try
+                    {
+                        var kernelName = ExecCommand("uname", "-s");
+                        curPlatform = (kernelName == "Darwin" ? PlatformID.MacOSX : platform);
+                    }
+                    catch (Win32Exception)
+                    { // probably no PATH to uname.
+                        curPlatform = platform;
+                    }
+                }
+            }
+            return curPlatform.Value;
+        }
+
+        private static PlatformID? curPlatform = null;
+
+    }
+}
---
+++ DynamicInterop/SafeHandleUnmanagedDll.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using Microsoft.Win32.SafeHandles;
+
+namespace DynamicInterop
+{
+    [SecurityPermission(SecurityAction.Demand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+    internal sealed class SafeHandleUnmanagedDll : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        public SafeHandleUnmanagedDll(IDynamicLibraryLoader libraryLoader) : base(true) 
+        {
+            if (libraryLoader != null) throw new ArgumentNullException("A dynamic library loader must be provided to this class", "libraryLoader");
+            this.libraryLoader = libraryLoader;
+        }
+
+        private readonly IDynamicLibraryLoader libraryLoader;
+
+        /// <summary>
+        /// Frees the native library this objects represents
+        /// </summary>
+        /// <returns>The result of the call to FreeLibrary</returns>
+        protected override bool ReleaseHandle()
+        {
+            return FreeLibrary();
+        }
+
+        private bool FreeLibrary()
+        {
+            bool freed = false;
+            if (libraryLoader == null)
+            {
+                if (!this.IsInvalid)
+                {
+                    try
+                    {
+                        throw new ApplicationException("Warning: unexpected condition of library loader and native handle - some native resources may not be properly disposed of");
+                    }
+                    finally
+                    {
+                        freed = false;
+                    }
+                }
+                else
+                    freed = true;
+                return freed;
+            }
+            else
+                return libraryLoader.FreeLibrary(handle);
+        }
+
+        public IntPtr GetFunctionAddress(string lpProcName)
+        {
+            return libraryLoader.GetFunctionAddress(handle, lpProcName);
+        }
+
+        /// <summary>
+        /// Frees the native library this objects represents
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            if (FreeLibrary())
+            {
+                SetHandleAsInvalid();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
