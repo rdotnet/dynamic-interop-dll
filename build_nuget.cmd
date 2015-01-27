666f5a8e463793c6d205ca7b2b2fd158198f991e
J-M <jean-michel.perraud@csiro.au>
2015-01-27 16:20:55 +1100

    Minor update of the windows batch build script

--- build_nuget.cmd
+++ build_nuget.cmd
@@ -5,4 +5,4 @@ set THIS_DIR=%~d0%~p0
 if not defined pkg_dir set pkg_dir=%THIS_DIR%
 
 set pack_options=-IncludeReferencedProjects -Verbosity normal -Properties Configuration=%BuildConfiguration% -OutputDirectory %pkg_dir%
-nuget pack %THIS_DIR%DynamicInterop\DynamicInterop.csproj %pack_options%
+nuget pack %THIS_DIR%DynamicInterop\DynamicInterop.nuspec %pack_options%
