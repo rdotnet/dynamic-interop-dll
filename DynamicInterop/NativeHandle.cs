d29cdcad00462e6e63c9866672dc1de12c98c18d
J-M <jean-michel.perraud@csiro.au>
2017-09-03 15:51:41 +1000

    Back up proj files prior to trying to move to netstandard2 target platform.

--- DynamicInterop/NativeHandle.cs
+++ DynamicInterop/NativeHandle.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Permissions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DynamicInterop
 {
--- DynamicInterop/SafeHandleUnmanagedDll.cs
+++ DynamicInterop/SafeHandleUnmanagedDll.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Permissions;
-using System.Text;
 using Microsoft.Win32.SafeHandles;
 
 namespace DynamicInterop
